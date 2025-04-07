f5ada06d208c9529d6e654ab4bdd70512bf98b2a
AxalotLDev <ladyginnic815@gmail.com>
2025-04-07 19:53:16 +0500

    fix: MobEntity loot dupe

--- src/main/java/com/axalotl/async/mixin/entity/MobEntityMixin.java
+++ src/main/java/com/axalotl/async/mixin/entity/MobEntityMixin.java
@@ -2,9 +2,8 @@ package com.axalotl.async.mixin.entity;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -16,31 +15,12 @@ public class MobEntityMixin {
     @Unique
     private static final ReentrantLock lock = new ReentrantLock();
 
-    @WrapMethod(method = "tryEquip")
-    private ItemStack tryEquip(ServerWorld world, ItemStack stack, Operation<ItemStack> original) {
+    @WrapMethod(method = "loot")
+    private void loot(ServerWorld world, ItemEntity itemEntity, Operation<Void> original) {
         synchronized (lock) {
-            return original.call(world, stack);
-        }
-    }
-
-    @WrapMethod(method = "equipStack")
-    private void equipStack(EquipmentSlot slot, ItemStack stack, Operation<Void> original) {
-        synchronized (lock) {
-            original.call(slot, stack);
-        }
-    }
-
-    @WrapMethod(method = "equipLootStack")
-    private void equipLootStack(EquipmentSlot slot, ItemStack stack, Operation<Void> original) {
-        synchronized (lock) {
-            original.call(slot, stack);
-        }
-    }
-
-    @WrapMethod(method = "equipBodyArmor")
-    private void equipLootStack(ItemStack stack, Operation<Void> original) {
-        synchronized (lock) {
-            original.call(stack);
+            if (!itemEntity.isRemoved()) {
+                original.call(world, itemEntity);
+            }
         }
     }
 }
